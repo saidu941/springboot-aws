7f07711fb41b3713df784feed12952887bc2b50b
saidu941 <saidu.dosapati@gmail.com>
2019-10-27 16:12:28 -0400

    added
    added

---
+++ springAWS2/src/main/java/com/telusko/secureapp/SpringAws2Application.java
@@ -0,0 +1,13 @@
+package com.telusko.secureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAws2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAws2Application.class, args);
+	}
+
+}
